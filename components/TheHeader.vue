aa32a9cb813917a5798fb647fdaf9784014d4932
DIego <mendesdiego90@gmail.com>
2023-12-22 00:29:27 +0900

    create the basic of the layout and header and footer file

---
+++ components/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h1>AKATSUKI</h1>
+        <nav>
+            <ul>
+                <li>
+                    Events
+                </li>
+                <li>
+                    Times
+                </li>
+                <li>
+                    Estatisticas
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+header {
+    width: 100vw;
+}
+</style>
---
+++ layout/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="layout">
+        <TheHeader />
+        <slot />
+        <footer>
+            here goes the footer
+        </footer>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import TheHeader from '../components/TheHeader.vue';
+
+
+
+</script>
+
+
+<style scoped>
+
+
+</style>
